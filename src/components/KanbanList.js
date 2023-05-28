bd585625e4d4c98528f368a799d69e3b26706cb7
shivampro1 <talkogramdd@gmail.com>
2023-05-28 18:14:04 +0530

    updated

--- src/components/KanbanList.js
+++ src/components/KanbanList.js
@@ -1,21 +1,41 @@
 import React from "react";
 import { useDrop } from "react-dnd";
 import KanbanTask from "./KanbanTask";
-
+import { KanbanContext } from "../contexts/KanbanContext";
+import { useContext } from "react";
 const KanbanList = ({ title, tasks }) => {
-  const [{ isOver }, drop] = useDrop({
+  const { kanban, setKanban } = useContext(KanbanContext);
+
+  const [, drop] = useDrop({
     accept: "task",
-    drop: (item) => console.log(`Dropped ${item.name} into ${title}`),
-    collect: (monitor) => ({
-      isOver: !!monitor.isOver(),
-    }),
+    drop: (item) => moveTask(item.id, title),
   });
 
+  const moveTask = (id, newStatus) => {
+    const newKanban = { ...kanban };
+    let task;
+    // Iterate over each status
+    Object.keys(newKanban).forEach((status) => {
+      newKanban[status] = newKanban[status].filter((t) => {
+        if (t.id === id) {
+          task = t;
+          return false;
+        }
+        return true;
+      });
+    });
+
+    if (task) {
+      newKanban[newStatus].push(task);
+      setKanban(newKanban);
+    }
+  };
+
   return (
-    <div ref={drop} className={`kanban-list ${isOver ? "kanban-list--over" : ""}`}>
+    <div ref={drop} className="kanban-list">
       <h2>{title}</h2>
-      {tasks.map((task, index) => (
-        <KanbanTask key={index} id={task.id} name={task.title} />
+      {tasks.map((task) => (
+        <KanbanTask key={task.id} id={task.id} name={task.title} status={title} />
       ))}
     </div>
   );
